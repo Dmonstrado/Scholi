09705c16981129807002960c58efb9af8e55f9c2
André Luiz <47571708+Dmonstrado@users.noreply.github.com>
2021-12-22 13:36:24 -0300

    Delete admin.py

--- ScholiProject/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(OA)
-admin.site.register(Tag)
-admin.site.register(Conteudo)
-admin.site.register(Disciplina)
